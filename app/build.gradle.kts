3f4812e208af811516229353b45c1bc58ed763f8
ItzFaisal <thisizfaisal@outlook.com>
2026-04-08 12:03:23 +0530

    updated libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,11 +27,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_18
-        targetCompatibility = JavaVersion.VERSION_18
-    }
-    kotlinOptions {
-        jvmTarget = "18"
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
 
